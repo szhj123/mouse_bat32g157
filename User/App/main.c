872d051bd6e9f4316fcb748134ad1635483a761b
szhj13 <64463561@qq.com>
2022-10-08 17:27:27 +0800

    build a new project

---
+++ User/App/main.c
@@ -0,0 +1,52 @@
+/********************************************************
+* @file       hardware.c
+* @author     szhj13
+* @version    V1.0
+* @date       2022-05-18
+* @brief      the start address of program
+**********************************************************
+* @Modify
+
+**********************************************************/
+
+/* Includes ---------------------------------------------*/
+#include "hal_bat32g157.h"
+/* Private typedef --------------------------------------*/
+/* Private define ---------------------------------------*/
+/* Private macro ----------------------------------------*/
+/* Private function -------------------------------------*/
+void Clk_Init(void );
+/* Private variables ------------------------------------*/
+
+int main (void)
+{
+		Clk_Init();
+	
+    Usb_Init(); 
+
+    while(1)
+    {
+        
+    }
+} 
+
+void Clk_Init(void )
+{
+    uint32_t waitCnt;
+    
+    //HOCO = 32MHz / fIH = 8MHz, UPLL = 48MHz, fclk = 64MHz
+    
+    CLK_Fclk_Select(MAINCLK_FIH);
+    CLK_PLL_Setting(PLL_SR_fIH,PLL_DIV_2,PLL_MUL_16);
+    CLK_UPLL_Setting(PLL_SR_fIH,PLL_DIV_2,PLL_MUL_12);
+    CLK_PLL_Start();
+    waitCnt = 10000;
+    while(waitCnt--);
+    CLK_UPLL_Start();
+    waitCnt = 10000;
+    while(waitCnt--);
+    CLK_Fclk_Select(MAINCLK_FPLL);
+
+    SystemCoreClock = 64000000;
+}
+
---
+++ User/Hal/hal_bat32g157.h
@@ -0,0 +1,19 @@
+#ifndef _HAL_BAT32G157_H
+#define _HAL_BAT32G157_H
+
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include "BAT32G157.h"
+#include "userdefine.h"
+#include "clk.h"
+#include "gpio.h"
+#include "usb.h"
+#include "dma.h"
+#include "spi.h"
+#include "tim4.h"
+#include "sci.h"
+
+#include "usb_phid_apl.h"
+
+#endif 
