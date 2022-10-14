c1ba4754a3f87348988e474bef0af0cab23b673c
szhj13 <64463561@qq.com>
2022-10-14 13:04:34 +0800

    continue to write key module

--- User/Drv/drv_key.h
+++ User/Drv/drv_key.h
@@ -3,6 +3,20 @@
 
 #include "hal_key.h"
 
+typedef struct _key_ctrl_block_t
+{
+    PORT_TypeDef port1;
+    PIN_TypeDef  pin1;
+    PORT_TypeDef port2;
+    PIN_TypeDef  pin2;
+
+    uint16_t     pressDelayCnt;
+    uint16_t     pressShortTime;
+    uint16_t     pressLongTime;
+
+    uint8_t      
+}
+
 void Drv_Key_Init(void );
 
 #endif
--- User/Hal/hal_key.c
+++ User/Hal/hal_key.c
@@ -21,5 +21,28 @@ void Hal_Key_Init(void )
 {
     //pd14, input, key1
     PORT_Init(PORTD, PIN14, INPUT);
+    //pd15, input, key2
+    PORT_Init(PORTD, PIN15, INPUT);
+    //pd02, input, key3
+    PORT_Init(PORTD, PIN2, INPUT);
+    //pd03, input, key4
+    PORT_Init(PORTD, PIN3, INPUT);
+    //pd04, input, key5
+    PORT_Init(PORTD, PIN4, INPUT);
+    //pd05, input, key6
+    PORT_Init(PORTD, PIN5, INPUT);
+    //pd06, input, key7
+    PORT_Init(PORTD, PIN6, INPUT);
+    //pd07, input, key8
+    PORT_Init(PORTD, PIN7, INPUT);
+    //pd08, input, key9
+    PORT_Init(PORTD, PIN8, INPUT);
 }
+
+void Hal_Key_Get_Value(PORT_TypeDef port, PIN_TypeDef pin )
+{
+    return PORT_GetBit(port, pin);
+}
+
+
 
