a2fbdbb2c014a684075866fad6d827131591e8f3
szhj13 <64463561@qq.com>
2022-11-06 21:39:48 +0800

    delete usb example

--- document/G157_USB/G157_USB/G157_USB_PUAC/G157_USB_PUAC/Examples/usb_audio/MDK/Objects/startup_bat32g157.d
+++
@@ -1,1 +0,0 @@
-.\objects\startup_bat32g157.o: RTE\Device\BAT32G157GK48FA\startup_BAT32G157.s
--- document/G157_USB/G157_USB/G157_USB_PUAC/G157_USB_PUAC/Examples/usb_audio/app/inc/sw_iic.h
+++
@@ -1,9 +0,0 @@
-#ifndef __SW_IIC_H__
-#define __SW_IIC_H__
-
-void IIC_Write_NByte(uint8_t devaddr,uint8_t* buf,uint8_t len);
-void IIC_Write(uint8_t devaddr,uint8_t regaddr,uint8_t* buf,uint8_t len);
-void IIC_Read(uint8_t devaddr,uint8_t regaddr,uint8_t* buf,uint8_t len);
-
-
-#endif
